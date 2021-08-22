f35c426e5e6d7f4b5bb84ddabe573a61bb2c86ff
Danilo Matos <dvalmont07@hotmail.com>
2021-08-22 14:03:12 -0300

    adding classes

---
+++ TheHive/Queen.cs
@@ -0,0 +1,11 @@
+﻿namespace TheHive
+{
+    public class Queen
+    {
+        private Worker[] _workers;
+        private int _shiftNumber;
+
+        public void AssingWork() { }
+        public void WorkTheNextShift() { }
+    }
+}
