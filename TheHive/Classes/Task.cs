03f02b6966b03bb8aa9677462c3fb12a04d97a09
Danilo Matos <dvalmont07@hotmail.com>
2021-08-22 14:46:59 -0300

    adding Task class

---
+++ TheHive/Classes/Task.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TheHive.Classes
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int ShiftsRequired { get; set; }
+    }
+}
