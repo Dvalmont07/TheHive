7cdbbfd73cb0d92448a2b392cafcff15fee32e8a
Danilo Matos <dvalmont07@hotmail.com>
2021-08-22 17:08:23 -0300

    Adding class

---
+++ TheHive/Classes/WorkerTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheHive.Classes
+{
+    public class WorkerTask
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public int ShiftsRequired { get; set; }
+    }
+
+}
