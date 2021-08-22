98b0a283f19cabad3513e360b3f07f13f3620148
Danilo Matos <dvalmont07@hotmail.com>
2021-08-22 17:20:06 -0300

    add

--- TheHive/Form1.Designer.cs
+++ TheHive/Form1.Designer.cs
@@ -33,10 +33,8 @@ namespace TheHive
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.assignJob = new System.Windows.Forms.Button();
-            this.shift = new System.Windows.Forms.ComboBox();
             this.tasks = new System.Windows.Forms.ComboBox();
             this.nextShift = new System.Windows.Forms.Button();
             this.report = new System.Windows.Forms.TextBox();
@@ -45,26 +43,15 @@ namespace TheHive
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.assignJob);
-            this.groupBox1.Controls.Add(this.shift);
             this.groupBox1.Controls.Add(this.tasks);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(374, 112);
+            this.groupBox1.Size = new System.Drawing.Size(256, 112);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Assignment of Workers Tasks ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(249, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Shift";
             // 
             // label1
             // 
@@ -84,20 +71,6 @@ namespace TheHive
             this.assignJob.Text = "Assign this job to a Worker";
             this.assignJob.UseVisualStyleBackColor = true;
             // 
-            // shift
-            // 
-            this.shift.FormattingEnabled = true;
-            this.shift.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.shift.Location = new System.Drawing.Point(249, 46);
-            this.shift.Name = "shift";
-            this.shift.Size = new System.Drawing.Size(119, 23);
-            this.shift.TabIndex = 1;
-            // 
             // tasks
             // 
             this.tasks.FormattingEnabled = true;
@@ -108,9 +81,9 @@ namespace TheHive
             // 
             // nextShift
             // 
-            this.nextShift.Location = new System.Drawing.Point(407, 24);
+            this.nextShift.Location = new System.Drawing.Point(274, 20);
             this.nextShift.Name = "nextShift";
-            this.nextShift.Size = new System.Drawing.Size(96, 88);
+            this.nextShift.Size = new System.Drawing.Size(96, 104);
             this.nextShift.TabIndex = 1;
             this.nextShift.Text = "Next Shift";
             this.nextShift.UseVisualStyleBackColor = true;
@@ -120,14 +93,14 @@ namespace TheHive
             this.report.Location = new System.Drawing.Point(12, 141);
             this.report.Multiline = true;
             this.report.Name = "report";
-            this.report.Size = new System.Drawing.Size(491, 210);
+            this.report.Size = new System.Drawing.Size(358, 207);
             this.report.TabIndex = 2;
             // 
             // Hive
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 360);
+            this.ClientSize = new System.Drawing.Size(383, 360);
             this.Controls.Add(this.report);
             this.Controls.Add(this.nextShift);
             this.Controls.Add(this.groupBox1);
@@ -145,9 +118,7 @@ namespace TheHive
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button nextShift;
         private System.Windows.Forms.Button assignJob;
-        private System.Windows.Forms.ComboBox shift;
         private System.Windows.Forms.ComboBox tasks;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox report;
     }
