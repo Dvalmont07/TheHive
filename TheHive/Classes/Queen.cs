703764c9eacb20a20e41fb8ccaf87d4279bc46e7
Danilo Matos <dvalmont07@hotmail.com>
2021-08-23 00:43:36 -0300

    Adding queen and worker classes

--- TheHive/Classes/Queen.cs
+++ TheHive/Classes/Queen.cs
@@ -1,11 +1,61 @@
-﻿namespace TheHive.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheHive.Classes
 {
     public class Queen
     {
-        private Worker[] _workers;
-        private int _shiftNumber;
+        private List<Worker> _workers;
+        public int ShiftNumber { get; set; }
+        public Queen(List<Worker> workers)
+        {
+            _workers = workers;
+        }
 
-        public void AssingWork() { }
-        public void WorkTheNextShift() { }
+        public MessageResult AssingWork(string job, int shifts)
+        {
+            MessageResult messageResult = new MessageResult { BoolResponse = false };
+
+            for (var i = 0; i < _workers.Count; i++)
+            {
+                if (string.IsNullOrEmpty(_workers[i].CurrentJob))
+                {
+                    messageResult.StringResponse += $"\n Worker {i + 1} {_workers[i].DoThisJob(job, shifts).StringResponse}";
+                }
+                else
+                {
+                    messageResult.StringResponse += $"\n The worker {i + 1} is currently assigned to another job";
+                }
+            }
+
+            return messageResult;
+
+        }
+        public MessageResult WorkTheNextShift()
+        {
+            StringBuilder result = new StringBuilder();
+            _ = result.AppendLine($"Report shift {ShiftNumber}");
+
+            for (var i = 0; i < _workers.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(_workers[i].CurrentJob))
+                {
+                    _workers[i].WorkOneShift();
+
+                    if (_workers[i].ShiftsLeft > 0)
+                    {
+                        _ = result.AppendLine($" --The Worker {i + 1} will do {_workers[i].CurrentJob} for more {_workers[i].ShiftsLeft} turns");
+                    }
+                    else
+                    {
+                        _ = result.AppendLine($" --The Worker {i + 1} finished the job");
+                    }
+                }
+            }
+
+            return new MessageResult { StringResponse = result.ToString() };
+
+        }
     }
 }
--- TheHive/Classes/Worker.cs
+++ TheHive/Classes/Worker.cs
@@ -1,14 +1,41 @@
-﻿namespace TheHive.Classes
+﻿using System.Collections.Generic;
+
+namespace TheHive.Classes
 {
     public class Worker
     {
-        private string[] _jobsICandDo;
+
         private int _shiftsToWork;
         private int _shiftsWorked;
-        public string CurrentJOb { get; set; }
+        public string CurrentJob { get; set; }
         public int ShiftsLeft { get; set; }
+        public List<string> JobsICandDo;
 
-        public void DoThisJob(){        }
-        public void WorkOneShift() { }
+        public MessageResult DoThisJob(string job, int shifts)
+        {
+                 if (!JobsICandDo.Contains(job))
+            {
+                return new MessageResult { BoolResponse = false, StringResponse = "can't do this job" };
+            }
+            else if (ShiftsLeft > 0)
+            {
+                return new MessageResult { BoolResponse = false, StringResponse = "is doing a assined job" };
+            }
+            else
+            {
+                CurrentJob = job;
+                ShiftsLeft = shifts;
+                return new MessageResult { BoolResponse = true, StringResponse = "has a new job was assigned" };
+            }
+        }
+        public void WorkOneShift()
+        {
+            ShiftsLeft--;
+
+            if (ShiftsLeft == 0)
+            {
+                CurrentJob = string.Empty;
+            }
+        }
     }
 }
